6ed1ece1819cf012bca22e465a3bfb462468776b
Omar <omar-chupanov@mail.ru>
2023-04-25 22:27:46 +0300

    hw

---
+++ hw 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void EvenNumber(int first, int second)
+{
+    if (first > second)
+    {
+        return;
+    }
+    if (first % 2 == 0)
+    {
+        Console.Write($"{first}, ");
+}
+EvenNumber(first+1,second);
+
+}
+
+EvenNumber(4,8);
+
